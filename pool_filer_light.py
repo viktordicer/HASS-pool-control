8f7cf4054b5eb48783711927c412e09a2bb68d81
viktor.dicer <viktor.dicer@gmail.com>
2022-07-04 18:02:32 +0200

    add ph pump

--- pool_filer_light.py
+++ pool_filer_light.py
@@ -1,11 +1,18 @@
-import smbus2
+import smbus
+import sys
+import getopt
+import time 
 import paho.mqtt.client as mqtt
+import threading
+from array import *
+from datetime import datetime
+import os
 
 i2c_addr = 0x20 #I2C address for devices
 i2c_register_out = [0x00, 0x01] #Set A and B to output
-i2c_register = [0x12, 0x13] # 0x12 is A register and 0x13 is B register
+i2c_register = [0x12, 0x13] # 0x012 is A register and 0x13 is B register
 
-bus = smbus2.SMBus(1)
+bus = smbus.SMBus(1)
 
 ##	SET REGISTER AS OUTPUTS
 #Device1
@@ -22,9 +29,11 @@ Broker = "192.168.0.107"
 sub_topic_filter = "command/pool/filter"
 sub_topic_light = "command/pool/light"
 sub_topic_state = "command/pool/state"
+sub_topic_ph = "command/pool/ph"
 
 pub_topic_filter = "sensor/pool/filter"
 pub_topic_light = "sensor/pool/light"
+pub_topic_ph = "sensor/pool/ph"
 
 #Set bit function	
 def set_bit(value, bit):
@@ -58,10 +67,10 @@ def on_message_filter(client, userdata, msg):
     reg_A = read_data(i2c_addr, i2c_register[0])
     if message == "0":
         write_data(i2c_addr,i2c_register[0],clear_bit(reg_A,2))
-        client.publish(pub_topic_filter, "off")
+        client.publish(pub_topic_filter, "Off")
     elif message == "1":
         write_data(i2c_addr,i2c_register[0],set_bit(reg_A,2))
-        client.publish(pub_topic_filter, "on")
+        client.publish(pub_topic_filter, "On")
 
 def on_message_light(client, userdata, msg):
     message = str(msg.payload.decode("utf-8"))
@@ -70,10 +79,22 @@ def on_message_light(client, userdata, msg):
     print(reg_A)
     if message == "0":
         write_data(i2c_addr,i2c_register[0],clear_bit(reg_A,1))
-        client.publish(pub_topic_light, "off")
+        client.publish(pub_topic_light, "Off")
     elif message == "1":
         write_data(i2c_addr,i2c_register[0],set_bit(reg_A,1))
-        client.publish(pub_topic_light, "on")
+        client.publish(pub_topic_light, "On")
+        
+def on_message_ph(client, userdata, msg):
+    message = str(msg.payload.decode("utf-8"))
+    print("message:" + message)
+    reg_A = read_data(i2c_addr, i2c_register[0])
+    print(reg_A)
+    if message == "0":
+        write_data(i2c_addr,i2c_register[0],clear_bit(reg_A,4))
+        client.publish(pub_topic_light, "Off")
+    elif message == "1":
+        write_data(i2c_addr,i2c_register[0],set_bit(reg_A,4))
+        client.publish(pub_topic_light, "On")
 
 def on_message_state(client, userdata, msg):
     message = str(msg.payload.decode("utf-8"))
@@ -81,11 +102,14 @@ def on_message_state(client, userdata, msg):
     reg_A = read_data(i2c_addr, i2c_register[0])
     client.publish(pub_topic_filter, is_bit_set(reg_A,2))
     client.publish(pub_topic_light, is_bit_set(reg_A,1))
+    client.publish(pub_topic_ph, is_bit_set(reg_A,4))
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.message_callback_add(sub_topic_filter, on_message_filter)
 client.message_callback_add(sub_topic_light, on_message_light)
+client.message_callback_add(sub_topic_ph, on_message_ph)
 client.message_callback_add(sub_topic_state, on_message_state)
+client.username_pw_set(username="viktor", password="viktor")
 client.connect(Broker, 1883, 60)
 client.loop_forever()
